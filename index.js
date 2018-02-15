3bdf1196e57b0a10744287d57efe9ba126dfa4cb
Mat Byczkowski <mbyczkowski@gmail.com>
2018-02-14 17:25:52 -0800

    proxy files from raw.githubusercontent.com

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-fly.http.respondWith((request) => {
-  return new Response("Hello! We support whirled peas.", {
-    status: 200
+const baseRepoUrl = 'https://raw.githubusercontent.com'
+
+fly.http.route("/:username/:repo/*path", function fileHandler(req, route) {
+  const params = route.params
+  return fetchFile(params.username, params.repo, params['*'])
+})
+
+fly.http.respondWith(async (req) => {
+  return new Response("Hello! We only support whirled peas.", {
+    status: 404
   })
 })
+
+async function fetchFile(username, repoName, filePath) {
+  const url = `${baseRepoUrl}/${username}/${repoName}/${filePath}`
+  return await fetch(url)
+}
