8c71150d54a39babb52a2b7d37bcd118e5ed09c0
Mat Byczkowski <mbyczkowski@gmail.com>
2018-02-23 11:29:32 -0800

    better script for deploying to Fly

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "On master branch, attempting to deploy to Fly..."
+  fly deploy
+  if [ $? != 0 ]; then
+    echo "Deploy Failed!"
+    exit 1
+  else
+    exit 0
+  fi
+else
+  echo "Not on master branch, skipping deploy to Fly."
+fi
